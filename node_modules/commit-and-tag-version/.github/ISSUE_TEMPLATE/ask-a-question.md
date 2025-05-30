869b8124ba3ce6235167558936673b2dcbca9a27
Thiago Silva <163438232+thidevops@users.noreply.github.com>
2025-05-29 23:17:29 -0300

    fix: removing node_modules

--- node_modules/commit-and-tag-version/.github/ISSUE_TEMPLATE/ask-a-question.md
+++
@@ -1,7 +0,0 @@
----
-name: Ask a Question
-about: '"How can I X?" – ask a question about how to use commit-and-tag-version.'
-title: ""
-labels: question
-assignees: ""
----
--- node_modules/commit-and-tag-version/test/mocks/updater/increment-updater.js
+++
@@ -1,7 +0,0 @@
-module.exports.readVersion = function (contents) {
-  return Number.parseInt(contents);
-};
-
-module.exports.writeVersion = function (contents) {
-  return this.readVersion(contents) + 1;
-};
